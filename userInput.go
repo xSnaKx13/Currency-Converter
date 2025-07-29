b8e702fd32a8234cc4b9f6c0891a4bd2ec73d72c
xSnaKx13 <152309157+xSnaKx13@users.noreply.github.com>
2025-07-29 07:22:53 +0000

    fixed the input

--- userInput.go
+++ userInput.go
@@ -14,6 +14,7 @@ func userInput(usingCurrency string, amount float64, requiredCurrency string) (s
 	fmt.Print("Выберете обменную валюту: ")
 	usingCurrency, err := bufio.NewReader(os.Stdin).ReadString('\n')
 	usingCurrency = strings.TrimSpace(usingCurrency)
+	strings.ToLower(usingCurrency)
 	if err != nil {
 		return "", 0, "", errors.New("Такой валюты нет!")
 	}
@@ -21,15 +22,16 @@ func userInput(usingCurrency string, amount float64, requiredCurrency string) (s
 	fmt.Print("Колличество, которое хотите обменять: ")
 	fmt.Scan(&amount)
 
-	fmt.Print("Выберете целевую валюту: ")
 	for _, elem := range allCurrency{
-		if strings.ToLower(requiredCurrency) == strings.ToLower(elem){
+		if usingCurrency == strings.ToLower(elem){
 			continue
 		}
 		fmt.Println(elem)
 	}
+	fmt.Println("Выберете целевую валюту: ")
 	requiredCurrency, err = bufio.NewReader(os.Stdin).ReadString('\n')
 	requiredCurrency = strings.TrimSpace(requiredCurrency)
+	strings.ToLower(requiredCurrency)
 	if err != nil {
 		return "", 0, "", errors.New("Такой валюты нет!")
 	}
